55941f030aecf0ba1a46d662cf8ea9d52e634d42
minsu kim <kimminsu31415@gmail.com>
2024-09-27 15:23:29 +0900

    add: 첫 페이지 디자인

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        Pretendard: ['Pretendard'],
-        SBAggro: ['SBAggro'],
+        Pretendard: ["Pretendard"],
+        SBAggro: ["SBAggro"],
       },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px", // breakpoint
+      xl: "1280px",
     },
   },
   plugins: [],
